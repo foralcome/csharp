b526d7c3ee0aa86616c6c71d298f1b69ec836dc0
foralcome <foralcome@mail.ru>
2022-08-06 11:27:34 +0700

    home task 2 search max number

---
+++ lesson1_hometask_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт максимальное.");
+
+Console.Write("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine("Число1 (" + num1 + ") больше числа2 (" + num2 + ")");
+}
+if (num1 < num2)
+{
+    Console.WriteLine("Число1 (" + num1 + ") меньше числа2 (" + num2 + ")");
+}
+if (num1 == num2)
+{
+    Console.WriteLine("Числа равны");
+}
