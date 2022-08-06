e13f7f67266a785cd641effef6b0f5a44f904503
foralcome <foralcome@mail.ru>
2022-08-06 11:57:55 +0700

    home task 8 even number in range 1 to N

---
+++ lesson1_hometask_8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.");
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for( int a = 2; a<num; a = a + 2)
+{
+    if( a!= 2 )
+        Console.Write(", ");
+    Console.Write(a);
+}
+Console.WriteLine();
+
+
