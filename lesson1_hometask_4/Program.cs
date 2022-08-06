9c53cf45f96056f9473e59678103d0e2de2a4345
foralcome <foralcome@mail.ru>
2022-08-06 11:35:08 +0700

    home task 4 search max

---
+++ lesson1_hometask_4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.");
+
+Console.Write("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+if (num2 > max)
+{
+    max = num2;
+}
+if (num3 > max)
+{
+    max = num3;
+}
+
+Console.WriteLine("Максимальное значение чисел равно: " + max);
