cafe595111f4a93b28c13a9971638268f4164c00
foralcome <foralcome@mail.ru>
2022-08-13 02:40:55 +0700

    Задание 19

---
+++ lesson3_hometask_19/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.");
+
+Console.Write("Введите 5-ти значное число: ");
+string numStr = Console.ReadLine();
+int num = int.Parse(numStr);
+int numLen = numStr.Length;
+
+if (numLen != 5)
+{
+    Console.WriteLine("Нужно 5-ти значное число!");
+}
+else
+{
+    int d = num;
+    int numCompare = 0;
+    int index = 0;
+    while (index < numLen)
+    {
+        numCompare = numCompare * 10 + d % 10;
+        d /= 10;
+        index++;
+    }
+
+    if (num == numCompare)
+    {
+        Console.WriteLine("Число является полиндромом!");
+    }
+    else
+    {
+        Console.WriteLine("Число НЕ является полиндромом!");
+    }
+}
