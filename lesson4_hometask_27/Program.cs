8c1972b2a0721ceba784d8b4a3330fc59fc01aa2
foralcome <foralcome@mail.ru>
2022-08-17 19:53:29 +0700

    Задание 27

---
+++ lesson4_hometask_27/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.");
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+int r = n;
+int sum = 0;
+while( r > 0 )
+{
+    sum += r % 10;
+    r /= 10;
+}
+
+Console.WriteLine($"Сумма цифр в числе {n} равна {sum}");
