32ef00d269f33b9d224d60c363b4e096b512d21c
foralcome <foralcome@mail.ru>
2022-08-17 20:05:11 +0700

    Задание 29

---
+++ lesson4_hometask_29/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.");
+
+int [] FillArray(int size=10)
+{
+    int []array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(1,99);
+    }
+    return array;
+}
+void PrintArray(int []array)
+{
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        if( i > 0 )
+        {
+            Console.Write(", ");
+        }
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine());
+
+if( size<=0 )
+{
+    Console.WriteLine("Размер массива должен быть больше 0!");
+}
+else
+{
+    int []array = FillArray(size);
+    PrintArray(array);
+}
