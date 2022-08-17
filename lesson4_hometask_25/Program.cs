6456f3771d8c8d6adbdb557e8eebc9fbe8bf7984
foralcome <foralcome@mail.ru>
2022-08-17 19:45:33 +0700

    Задание 25

---
+++ lesson4_hometask_25/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.");
+
+int Degree(int number, int degree)
+{
+    int result = 1;
+    for (int i = 1; i <= degree; i++)
+    {
+        result *= number;
+    }
+    return result;
+}
+
+Console.Write("Введите число A: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+
+if(b <= 0){
+    Console.WriteLine("Число B должно быть больше 0!");
+}
+else
+{
+    int result = Degree(a,b);
+    Console.WriteLine($"Результат: {result}");
+}
