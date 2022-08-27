fdbe50ed24664a259b683f64fc34770ab4a2356a
foralcome <foralcome@mail.ru>
2022-08-27 14:20:37 +0700

    Задание 47 - modify

--- lesson7_hometask_47/Program.cs
+++ lesson7_hometask_47/Program.cs
@@ -1,22 +1,22 @@
 ﻿Console.WriteLine("Задача 47. Задайте двумерный массив размером m x n, заполненный случайными вещественными числами.");
 
-int [,] FillArray2dRundom(int countRows=3, int countColumns=3, int minRandomValue=1, int maxRandomValue=100)
+int[,] FillArray2dRandom(int countRows = 3, int countColumns = 3, int minRandomValue = 1, int maxRandomValue = 100)
 {
-    if( countRows<=0 || countColumns<=0 )
+    if (countRows <= 0 || countColumns <= 0)
     {
         throw new ArgumentException("Размерность массива должна быть больше 0!");
     }
-    int [,]array = new int[countRows,countColumns];
+    int[,] array = new int[countRows, countColumns];
     for (int i = 0; i < countRows; i++)
     {
         for (int j = 0; j < countColumns; j++)
         {
-            array[i,j] = new Random().Next(minRandomValue,maxRandomValue);
+            array[i, j] = new Random().Next(minRandomValue, maxRandomValue);
         }
     }
     return array;
 }
-void PrintArray2d(int [,]array)
+void PrintArray2d(int[,] array)
 {
     Console.Write(" ".PadLeft(5));
     for (int i = 0; i < array.GetLength(1); i++)
@@ -30,7 +30,7 @@ void PrintArray2d(int [,]array)
         Console.Write($"{i}".PadLeft(5));
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write($"{array[i,j]}".PadLeft(5));
+            Console.Write($"{array[i, j]}".PadLeft(5));
         }
         Console.WriteLine("");
     }
@@ -42,12 +42,12 @@ int countRows = int.Parse(Console.ReadLine());
 Console.Write("Укажите количество столбцов: ");
 int countColumns = int.Parse(Console.ReadLine());
 
-if( countRows<=0 || countColumns<=0 )
+if (countRows <= 0 || countColumns <= 0)
 {
     Console.WriteLine("Размерность массива должна быть больше 0!");
 }
 else
 {
-    int [,]array2d = FillArray2dRundom(countRows, countColumns, 10, 99);
+    int[,] array2d = FillArray2dRandom(countRows, countColumns, 10, 99);
     PrintArray2d(array2d);
 }
