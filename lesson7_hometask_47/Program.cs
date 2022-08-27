b9bf933bbcaabd796ccd5f519439589ecd8a96f2
foralcome <foralcome@mail.ru>
2022-08-27 14:19:14 +0700

    Задание 47

---
+++ lesson7_hometask_47/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.WriteLine("Задача 47. Задайте двумерный массив размером m x n, заполненный случайными вещественными числами.");
+
+int [,] FillArray2dRundom(int countRows=3, int countColumns=3, int minRandomValue=1, int maxRandomValue=100)
+{
+    if( countRows<=0 || countColumns<=0 )
+    {
+        throw new ArgumentException("Размерность массива должна быть больше 0!");
+    }
+    int [,]array = new int[countRows,countColumns];
+    for (int i = 0; i < countRows; i++)
+    {
+        for (int j = 0; j < countColumns; j++)
+        {
+            array[i,j] = new Random().Next(minRandomValue,maxRandomValue);
+        }
+    }
+    return array;
+}
+void PrintArray2d(int [,]array)
+{
+    Console.Write(" ".PadLeft(5));
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        Console.Write($"{i}".PadLeft(5));
+    }
+    Console.WriteLine();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write($"{i}".PadLeft(5));
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]}".PadLeft(5));
+        }
+        Console.WriteLine("");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Укажите количество строк: ");
+int countRows = int.Parse(Console.ReadLine());
+Console.Write("Укажите количество столбцов: ");
+int countColumns = int.Parse(Console.ReadLine());
+
+if( countRows<=0 || countColumns<=0 )
+{
+    Console.WriteLine("Размерность массива должна быть больше 0!");
+}
+else
+{
+    int [,]array2d = FillArray2dRundom(countRows, countColumns, 10, 99);
+    PrintArray2d(array2d);
+}
