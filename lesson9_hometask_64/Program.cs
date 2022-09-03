622dfde952c3f066b35a1c7b85537add342fbdc2
foralcome <foralcome@mail.ru>
2022-09-03 09:01:03 +0700

    Задание 64 - add recursion print

--- lesson9_hometask_64/Program.cs
+++ lesson9_hometask_64/Program.cs
@@ -2,19 +2,36 @@
 
 void PrintRange(int numberStart, int numberStop)
 {
-    if(numberStart > numberStop){
+    if (numberStart > numberStop)
+    {
         throw new ArgumentException($"Число {numberStart} должно быть меньше {numberStop}!");
     }
 
     for (int i = numberStart; i <= numberStop; i++)
     {
-        if(i!=numberStart)
+        if (i != numberStart)
         {
             Console.Write(",");
         }
         Console.Write($"{i}");
     }
     Console.WriteLine();
+}
+void PrintRangeRecursion(int curentNumber, int stopNumber = 1)
+{
+    if (curentNumber < stopNumber)
+    {
+        throw new ArgumentException($"Текущее число {curentNumber} должно быть больше или равно {stopNumber}!");
+    }
+    if (curentNumber == stopNumber)
+    {
+        Console.Write($"{curentNumber}");
+    }
+    else
+    {
+        PrintRangeRecursion(curentNumber - 1);
+        Console.Write($",{curentNumber}");
+    }
 }
 
 try
@@ -25,9 +42,13 @@ try
     Console.Write("Введите число N: ");
     int n = int.Parse(Console.ReadLine());
 
-    PrintRange(m,n);
+    Console.WriteLine($"Обычный вывод диапазона [{m},{n}]");
+    PrintRange(m, n);
+    Console.WriteLine($"Рекурсивный вывод диапазона [{m},{n}]");
+    PrintRangeRecursion(n, m);
+    Console.WriteLine();
 }
-catch(Exception ex)
+catch (Exception ex)
 {
     Console.WriteLine($"Произошло исключение: {ex.Message}");
 }
